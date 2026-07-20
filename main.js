a63197fcddbf400634260c014d08add03f7fa134
devendrasai05 <devendrasai05@gmail.com>
2026-07-20 19:22:47 +0530

    Integrate React with Electron using Vite

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 1000,
-        height: 700,
-        title: "OfflineNet"
-    });
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    title: "OfflineNet",
+  });
 
-    win.loadFile("index.html");
+  win.loadURL("http://localhost:5173");
 }
 
 app.whenReady().then(() => {
-    createWindow();
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>OfflineNet</h1>
+      <p>My first React application inside Electron 🚀</p>
+    </div>
+  );
+}
+
+export default App;
