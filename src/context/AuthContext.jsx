69f43138a07dc49371c117113e2dc02800163f58
devendrasai05 <devendrasai05@gmail.com>
2026-07-21 17:45:48 +0530

    Add persistent aunthentication with protected routes

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,20 +1,39 @@
-import { createContext, useContext, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 const AuthContext = createContext();
 
 function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+  const savedUser = localStorage.getItem("offlinenet-user");
+  return savedUser ? JSON.parse(savedUser) : null;
+  });
 
   const login = (userData) => {
+    localStorage.setItem(
+      "offlinenet-user",
+      JSON.stringify(userData)
+    );
+
+    setUser(userData);
+  };
+
+  const register = (userData) => {
+    localStorage.setItem(
+      "offlinenet-user",
+      JSON.stringify(userData)
+    );
+
     setUser(userData);
   };
 
   const logout = () => {
+    localStorage.removeItem("offlinenet-user");
     setUser(null);
-  };
-
-  const register = (userData) => {
-    setUser(userData);
   };
 
   return (
@@ -22,8 +41,8 @@ function AuthProvider({ children }) {
       value={{
         user,
         login,
-        logout,
         register,
+        logout,
       }}
     >
       {children}
